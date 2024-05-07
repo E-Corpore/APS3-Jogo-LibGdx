ce86d41cd8817d6e415223ce92be01836f0991aa
edward-d3v <42708123+edward-d3v@users.noreply.github.com>
2024-05-06 23:13:46 -0300

    Commit inicial

---
+++ desktop/src/com/aps3/jogo/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.aps3.jogo;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.aps3.jogo.Jogo;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("APS3-Jogo-LibGdx");
+		new Lwjgl3Application(new Jogo(), config);
+	}
+}
